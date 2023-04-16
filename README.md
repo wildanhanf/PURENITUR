12458864da95bfeb348e629fbc3c569a2405213c
rhezapandya <rheza20001@mail.unpad.ac.id>
2023-04-16 20:39:55 +0700

    init laravel login register

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# PURENITUR
+### PURENITUR
+
+# Project Proyek Perangkat Lunak 1
+# Kelompok ArtinyaApaBangMessi
+
+# Anggota :
+#    1. Affan Rifqy
+#    2. Rheza Pandya
+#    3. Wildan Hanif
+#    4. Andyka Baswara
---
+++ resources/views/layouts/app-master.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.87.0">
+    <title>Fixed top navbar example · Bootstrap v5.1</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{!! url('assets/bootstrap/css/bootstrap.min.css') !!}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="{!! url('assets/css/app.css') !!}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    @include('layouts.partials.navbar')
+
+    <main class="container">
+        @yield('content')
+    </main>
+
+    <script src="{!! url('assets/bootstrap/js/bootstrap.bundle.min.js') !!}"></script>
+
+</body>
+
+</html>
