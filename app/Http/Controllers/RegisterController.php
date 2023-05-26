c62bf9b298c23b02bcc9ef88b34140ce9e6b839c
rhezapandya <rheza20001@mail.unpad.ac.id>
2023-05-26 18:22:05 +0700

    [BE - Rheza] Benerin Register Request

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
@@ -26,10 +27,23 @@ class RegisterController extends Controller
      * 
      * @return \Illuminate\Http\Response
      */
-    public function register(RegisterRequest $request)
+    public function register(Request $request): RedirectResponse
     {
         // dd($request);
-        $user = User::create($request->validated([
+
+        $request->validate([
+            'email' => 'required|email:rfc,dns|unique:users,email',
+            'username' => 'required|unique:users,username|min:4|max:16',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'gender' => 'required|max:1',
+            'address' => 'required',
+            'telephone' => 'required|max:14'
+        ]);
+
+        $user = User::create([
             'email' => $request->email,
             'username' => $request->username,
             'password' => Hash::make($request->password),
@@ -38,7 +52,7 @@ class RegisterController extends Controller
             'gender' => $request->gender,
             'address' => $request->address,
             'telephone' =>  $request->telephone,
-        ]));
+        ]);
 
         auth()->login($user);
 
